6d1d3bf72190930b1efe52a88b66ce8eef2129d3
Lee SeungJae <147023976+dltmdwoOS@users.noreply.github.com>
2026-01-09 15:26:31 +0900

    clean code

--- oracle_research.sh
+++ oracle_research.sh
@@ -1,11 +1,11 @@
 export TEST_MODE=True
 
 python3 main_aro.py \
-    --dataset COCO_QA_one_obj \
+    --dataset Controlled_Images_A \
     --model-name='llava1.5' \
     --download \
-    --method adapt_vis_var_uncertainties_var_weights \
+    --method adapt_vis_for_oracle_research \
     --weight1 0.5  \
-    --weight2 1.2 \
-    --threshold 0.3 \
+    --weight2 1.5 \
+    --threshold 0.4 \
     --option=four
